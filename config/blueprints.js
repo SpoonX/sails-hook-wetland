022a8f14f79d388750f2e00b2de6c4af64c53f4c
RWOverdijk <r.w.overdijk@gmail.com>
2017-08-03 20:56:49 +0200

    chore(project): default header to false for bc

--- config/blueprints.js
+++ config/blueprints.js
@@ -1,7 +1,7 @@
 module.exports.blueprints = {
 
   // Enabling this options will include the total count for the current query in the results using the value as header key.
-  countInResponse: 'X-Total-Count',
+  countInResponse: false,
 
   // This option allows you to nest the response.
   dataProperty: false
